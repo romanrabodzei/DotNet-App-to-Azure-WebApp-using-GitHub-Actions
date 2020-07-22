6634469023bcada086a06ee2f01177ff8750cec7
romanrabodzei <romanrabodzei@noreply.github.com>
2020-07-22 12:21:07 +0300

    buld and deploy webapp

--- CreatingWebApp.ps1
+++ CreatingWebApp.ps1
@@ -3,10 +3,18 @@ Connect-AzAccount
 
 #Create Resource Group
 New-AzResourceGroup -Name WebAppRG -Location canadacentral -Tag @{ "Env"="WebApp"; "Owner"="Roman" }
-
+$ScopeId = (Get-AzResourceGroup -Name WebAppRG).ResourceId
 
 #Create an application service plan
 New-AzAppServicePlan -Name WebAppSP -ResourceGroupName WebAppRG -Location canadacentral -Tier Free
 New-AzWebApp -Name WebApp1793 -ResourceGroupName WebAppRG -Location canadacentral -AppServicePlan WebAppSP
-Stop-AzWebApp -Name WebApp1793
 
+#Creating a role assignment
+az login
+az ad sp create-for-rbac --name WebAppSvcAcc --role Contributor --scopes "$ScopeId" --sdk-auth
+
+#Create ASP.NET core application
+dotnet new WebApp
+
+#Check .Net version
+dotnet --version
