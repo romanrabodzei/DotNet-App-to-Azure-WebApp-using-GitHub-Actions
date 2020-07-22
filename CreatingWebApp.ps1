b3b95f7d850e10a2a9fe1971a68a6300f1f961f8
romanrabodzei <romanrabodzei@noreply.github.com>
2020-07-22 11:29:05 +0300

    Update 001

--- CreatingWebApp.ps1
+++ CreatingWebApp.ps1
@@ -1,3 +1,12 @@
 #Login To Azure
 Connect-AzAccount
 
+#Create Resource Group
+New-AzResourceGroup -Name WebAppRG -Location canadacentral -Tag @{ "Env"="WebApp"; "Owner"="Roman" }
+
+
+#Create an application service plan
+New-AzAppServicePlan -Name WebAppSP -ResourceGroupName WebAppRG -Location canadacentral -Tier Free
+New-AzWebApp -Name WebApp1793 -ResourceGroupName WebAppRG -Location canadacentral -AppServicePlan WebAppSP
+Stop-AzWebApp -Name WebApp1793
+
